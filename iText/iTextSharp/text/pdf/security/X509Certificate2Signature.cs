3f602ed787d2cbcf74e828800b159d8ef7ebd124
Lorenzo Talamelli <lorenzo.talamelli@ericsoft.com>
2018-11-09 12:02:06 +0100

    Fix firma con certificato

--- iText/iTextSharp/text/pdf/security/X509Certificate2Signature.cs
+++ iText/iTextSharp/text/pdf/security/X509Certificate2Signature.cs
@@ -1,15 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
-namespace iTextSharp.text.pdf.security {
+namespace iTextSharp.text.pdf.security
+{
     /// <summary>
     /// Creates a signature using a X509Certificate2. It supports smartcards without 
     /// exportable private keys.
     /// </summary>
-    public class X509Certificate2Signature : IExternalSignature {
+    public class X509Certificate2Signature : IExternalSignature
+    {
         /// <summary>
         /// The certificate with the private key
         /// </summary>
@@ -18,7 +18,7 @@ namespace iTextSharp.text.pdf.security {
         private String hashAlgorithm;
         /** The encryption algorithm (obtained from the private key) */
         private String encryptionAlgorithm;
-        
+
         /// <summary>
         /// Creates a signature using a X509Certificate2. It supports smartcards without 
         /// exportable private keys.
@@ -26,7 +26,8 @@ namespace iTextSharp.text.pdf.security {
         /// <param name="certificate">The certificate with the private key</param>
         /// <param name="hashAlgorithm">The hash algorithm for the signature. As the Windows CAPI is used
         /// to do the signature the only hash guaranteed to exist is SHA-1</param>
-        public X509Certificate2Signature(X509Certificate2 certificate, String hashAlgorithm) {
+        public X509Certificate2Signature(X509Certificate2 certificate, String hashAlgorithm)
+        {
             if (!certificate.HasPrivateKey)
                 throw new ArgumentException("No private key.");
             this.certificate = certificate;
@@ -39,16 +40,19 @@ namespace iTextSharp.text.pdf.security {
                 throw new ArgumentException("Unknown encryption algorithm " + certificate.PrivateKey);
         }
 
-        public virtual byte[] Sign(byte[] message) {
-            if (certificate.PrivateKey is RSACryptoServiceProvider) {
+        public virtual byte[] Sign(byte[] message)
+        {
+            if (certificate.PrivateKey is RSACryptoServiceProvider provider)
+            {
                 // Force rsa all PrivateKey properties 
                 //Including  Private Parameters  
                 //Instead of cast  that does not always work
-                RSACryptoServiceProvider rsa = new   RSACryptoServiceProvider() ;
-                rsa.FromXmlString(certificate.PrivateKey.ToXmlString(true)) ;      
-                return rsa.SignData(message, hashAlgorithm);
+                //RSACryptoServiceProvider rsa = certificate.PrivateKey; new   RSACryptoServiceProvider() ;
+                //rsa.FromXmlString(certificate.PrivateKey.ToXmlString(true)) ;      
+                return provider.SignData(message, hashAlgorithm);
             }
-            else {
+            else
+            {
                 DSACryptoServiceProvider dsa = (DSACryptoServiceProvider)certificate.PrivateKey;
                 return dsa.SignData(message);
             }
@@ -59,16 +63,18 @@ namespace iTextSharp.text.pdf.security {
          * @return  the hash algorithm (e.g. "SHA-1", "SHA-256,...")
          * @see com.itextpdf.text.pdf.security.ExternalSignature#getHashAlgorithm()
          */
-        public virtual String GetHashAlgorithm() {
+        public virtual String GetHashAlgorithm()
+        {
             return hashAlgorithm;
         }
-        
+
         /**
          * Returns the encryption algorithm used for signing.
          * @return the encryption algorithm ("RSA" or "DSA")
          * @see com.itextpdf.text.pdf.security.ExternalSignature#getEncryptionAlgorithm()
          */
-        public virtual String GetEncryptionAlgorithm() {
+        public virtual String GetEncryptionAlgorithm()
+        {
             return encryptionAlgorithm;
         }
     }
