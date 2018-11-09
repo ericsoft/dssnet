0e6242d164fc4f77dca3464fbd6479387858be77
Lorenzo Talamelli <lorenzo.talamelli@ericsoft.com>
2018-11-09 12:59:34 +0100

    Reso disposable FileDocument

--- dss-document/Signature/FileDocument.cs
+++ dss-document/Signature/FileDocument.cs
@@ -18,61 +18,71 @@
  * "DSS - Digital Signature Services".  If not, see <http://www.gnu.org/licenses/>.
  */
 
+using System;
 using System.IO;
 using EU.Europa.EC.Markt.Dss.Signature;
 using Sharpen;
 
 namespace EU.Europa.EC.Markt.Dss.Signature
 {
-	/// <summary>Document implementation stored on file-system.</summary>
-	/// <remarks>Document implementation stored on file-system.</remarks>
-	/// <version>$Revision: 1887 $ - $Date: 2013-04-23 14:56:09 +0200 (mar., 23 avr. 2013) $
-	/// 	</version>
-	public class FileDocument : Document
-	{
-		private FilePath file;
+    /// <summary>Document implementation stored on file-system.</summary>
+    /// <remarks>Document implementation stored on file-system.</remarks>
+    /// <version>$Revision: 1887 $ - $Date: 2013-04-23 14:56:09 +0200 (mar., 23 avr. 2013) $
+    /// 	</version>
+    public class FileDocument : Document, IDisposable
+    {
+        private FilePath file;
+        private Stream ms;
 
-		/// <summary>Create a FileDocument</summary>
-		/// <param name="pathname"></param>
-		public FileDocument(string pathname) : this(new FilePath(pathname))
-		{
-		}
 
-		/// <summary>Create a FileDocument</summary>
-		/// <param name="file"></param>
-		public FileDocument(FilePath file)
-		{
-			if (!file.Exists())
-			{
-				throw new RuntimeException("File Not Found");
-			}
-			this.file = file;
-		}
+        /// <summary>Create a FileDocument</summary>
+        /// <param name="pathname"></param>
+        public FileDocument(string pathname) : this(new FilePath(pathname))
+        {
+        }
 
-		/// <exception cref="System.IO.IOException"></exception>
-		public virtual Stream OpenStream()
-		{            
-			return File.OpenRead(file);
-		}
+        /// <summary>Create a FileDocument</summary>
+        /// <param name="file"></param>
+        public FileDocument(FilePath file)
+        {
+            if (!file.Exists())
+            {
+                throw new RuntimeException("File Not Found");
+            }
+            this.file = file;
+        }
 
-		public virtual bool Exists()
-		{
-			return file.Exists();
-		}
+        /// <exception cref="System.IO.IOException"></exception>
+        public virtual Stream OpenStream()
+        {
+            this.ms = File.OpenRead(file);
+            return ms;
+        }
 
-		public virtual FilePath GetParentFile()
-		{
-			return file.GetParentFile();
-		}
+        public virtual bool Exists()
+        {
+            return file.Exists();
+        }
 
-		public virtual string GetName()
-		{
-			return file.GetName();
-		}
+        public virtual FilePath GetParentFile()
+        {
+            return file.GetParentFile();
+        }
 
-		public virtual MimeType GetMimeType()
-		{
-			return MimeType.FromFileName(GetName());
-		}
-	}
+        public virtual string GetName()
+        {
+            return file.GetName();
+        }
+
+        public virtual MimeType GetMimeType()
+        {
+            return MimeType.FromFileName(GetName());
+        }
+
+        public void Dispose()
+        {
+            this.ms?.Dispose();
+            file.Delete();
+        }
+    }
 }
